93539db8e1ebfae1829d4c51baa4fff4d3d79e30
Carlos Eduardo Vaz <carlos.voliveira@al.infnet.edu.br>
2021-03-12 10:34:19 -0300

    estabelendo rota

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const cors = require('cors');
+const router = require('./routes/router');
 
 //TODO: configurando o servidor
 
 app.use(express.json());
 app.use(cors());
 
+router(app);
 //TODO: levantando o server
 
 const port = 3001;
